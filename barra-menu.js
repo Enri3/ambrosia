760eea856b4930693481a78ee710a5e0d44077e9
Enri3 <ereschini06@gmail.com>
2022-05-13 11:33:58 -0300

    Laburar con esto

---
+++ barra-menu.js
@@ -0,0 +1,4 @@
+function prueba() {
+   document.getElementByClassName('barra-menu-buttom').classList.add ('pruebajsadd');
+   document.getElementByClassName('barra-menu-buttom').classList.remove ('links');
+}
