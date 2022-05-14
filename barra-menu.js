c755f89b0d46628b8ed1954ec74e25c4bd433e87
Enri3 <92584993+Enri3@users.noreply.github.com>
2022-05-14 17:52:56 -0300

    Delete barra-menu.js

--- barra-menu.js
+++
@@ -1,4 +0,0 @@
-function prueba() {
-   document.getElementByClassName('barra-menu-buttom').classList.add ('pruebajsadd');
-   document.getElementByClassName('barra-menu-buttom').classList.remove ('links');
-}
